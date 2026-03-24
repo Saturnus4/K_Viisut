ca105a15499ddb645e0a8adc2587a81c0f369436
Saturnus4 <riko.koivumaki@gmail.com>
2026-03-24 19:44:01 +0200

    Port bug continues

--- K_viisut.py
+++ K_viisut.py
@@ -114,6 +114,6 @@ def results():
 
 
 # Remove this entirely for Render
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
-#     app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == "__main__":
+     port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+     app.run(host="0.0.0.0", port=port) # debug=True
